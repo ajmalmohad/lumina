c9289b9a49e662124d12638663ab2df24e46096e
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-08-30 14:47:28 +0530

    Transfer Card

--- src/components/TransferCard.tsx
+++ src/components/TransferCard.tsx
@@ -1,18 +1,26 @@
-import {Card, CardHeader, Image} from "@nextui-org/react";
+import {Card, CardBody, CardHeader, Input, Chip} from "@nextui-org/react";
+import { useBlockchainStore } from "../store/store";
+import { useState } from "react";
 
 function TransferCard() {
+  const { wallet } = useBlockchainStore()
+
+  const [toAddress, setAddress] = useState("");
+  const [amount, setAmount] = useState("");
+
   return (
     <>
         <Card isBlurred className="border-none bg-background/60 dark:bg-default-100/50 col-span-12 sm:col-span-6 h-[500px]">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+          <CardHeader>
             <h4 className="text-white font-medium text-large">Transfer</h4>
           </CardHeader>
-          <Image
-            removeWrapper
-            alt="Card background"
-            className="z-0 w-full h-full object-cover"
-            src="/images/card-example-4.jpeg"
-          />
+          <CardBody>
+            <Input isClearable type="text" variant={"bordered"} label="To Address" value={toAddress} onValueChange={setAddress} />
+            <Input className="mt-5" type="number" variant={"bordered"} label="Amount" value={amount} onValueChange={setAmount} />
+            <div className="mt-5">
+              <Chip color="default">{wallet.balance - (parseFloat(amount) || 0) >= 0 ? "Remaining: "+ (wallet.balance - (parseFloat(amount) || 0)).toString() : "Invalid" }</Chip>
+            </div>
+          </CardBody>
         </Card>
     </>
   )

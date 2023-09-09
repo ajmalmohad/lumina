08de093a55c55a3b79a205cbd2e0477ed1b64a48
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-09-09 14:09:18 +0530

    History Card and Persist the Block Chain

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -1,12 +1,15 @@
-import {Card, CardHeader} from "@nextui-org/react";
+import {Card, CardBody, CardHeader} from "@nextui-org/react";
 
 function HistoryCard() {
   return (
     <>
         <Card isBlurred className="border-none bg-background/60 dark:bg-default-100/50 h-[500px] col-span-12 sm:col-span-4">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+          <CardHeader>
             <h4 className="text-white font-medium text-large">Transaction History</h4>
           </CardHeader>
+          <CardBody>
+            Coming Soon...
+          </CardBody>
         </Card>
     </>
   )
